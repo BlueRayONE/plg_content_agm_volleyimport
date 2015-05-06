261cc67632ba63ef7a3747d9a4f68187dfa8f29c
Alexander Grözinger <agr@agmedia.de>
2015-05-06 14:24:39 +0200

    Update volleyimport_CHANGELOG.php

--- volleyimport_CHANGELOG.php
+++ volleyimport_CHANGELOG.php
@@ -45,9 +45,9 @@ ______________________________________________
 16-Mar-2015 Alexander Grözinger
  ! Startup
 06-May-2015
- # Sortierung nach Datum unklar: DataTable's Lösung durch einbinden der moment.js Bibliothek. Automatische Erkennung 
+ #3 Sortierung nach Datum unklar: DataTable's Lösung durch einbinden der moment.js Bibliothek. Automatische Erkennung 
        des deutschen Datumformats hinzugefügt: DD.MM.YYYY
- # Tabelle in horizontaler Richtung größer als zur Verfügung stehender Platz: Problem behoben indem Tabelle nun 
+ #1 Tabelle in horizontaler Richtung größer als zur Verfügung stehender Platz: Problem behoben indem Tabelle nun 
        horizontal scrollbar wird sobald der Platz nicht mehr ausreicht.
        + -> geplant ist eine Funktion um die anzuzeigenden Spalten dynamisch ein- und ausblenden zu können
 
